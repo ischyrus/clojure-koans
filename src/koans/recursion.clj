095ecef84a24803226759e9e018425d074050f44
Steven Schermerhorn <stevens@ischyrus.com>
2011-10-19 18:57:08 -0700

    Did some more koans

--- src/koans/recursion.clj
+++ src/koans/recursion.clj
@@ -1,20 +1,27 @@
 (defn is-even? [n]
   (if (= n 0)
-      __
-      (___ (is-even? (dec n)))))
+      true
+      (not (is-even? (dec n)))))
 
 (defn is-even-bigint? [n]
   (loop [n   n
          acc true]
     (if (= n 0)
-        __
+        false
         (recur (dec n) (not acc)))))
 
 (defn recursive-reverse [coll]
-  __)
+  (loop [coll coll acc ()]
+   (if (seq coll)
+    (recur (rest coll) (conj acc (first coll)))
+    acc)))
 
-(defn factorial [n]
-  __)
+    (defn factorial [n]
+    (bigint n)
+      (loop [cnt n acc 1]
+        (if (= cnt 1)
+          acc
+        (recur (- cnt 1) (* cnt acc)))))
 
 (meditations
   "Recursion ends with a base case"
