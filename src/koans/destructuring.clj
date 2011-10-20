c98614effe0ff872900a1a310aef5c23ca9347d6
Steven Schermerhorn <stevens@ischyrus.com>
2011-10-19 21:28:06 -0700

    a couple more

--- src/koans/destructuring.clj
+++ src/koans/destructuring.clj
@@ -29,14 +29,14 @@
   "Break up maps by key"
   (= "123 Test Lane, Testerville, TX"
      (let [{street-address :street-address, city :city, state :state} test-address]
-            ))
+       (str street-address ", " city ", " state)))
 
   "Or more succinctly"
   (= "123 Test Lane, Testerville, TX"
-     (let [{:keys [street-address __ __]} test-address]
-            __))
+     (let [{:keys [street-address city state]} test-address]
+            (str street-address ", " city ", " state)))
 
   "All together now!"
   (= "Test Testerson, 123 Test Lane, Testerville, TX"
-     (___ ["Test" "Testerson"] test-address))
+     (__ ["Test" "Testerson"] test-address))
 )
